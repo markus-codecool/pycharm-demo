940aeec49a9e8623be7a20da8c2a4e901270da36
markus-lehr <markus.lehr@codecool.com>
2022-03-09 13:44:21 +0100

    Initial commit

---
+++ src/controller.py
@@ -0,0 +1,18 @@
+def function1():
+    #  TODO implement this
+    pass
+
+
+def function2():
+    #  TODO implement this
+    pass
+
+
+def function3():
+    #  TODO implement this
+    pass
+
+
+def function4():
+    #  TODO implement this
+    pass
